e31e3e680f63d19a52b3b06a45a1cd0857d43c24
Dustin heaps <dustinheaps89@gmail.com>
2019-06-13 21:06:43 -0400

    changes

---
+++ src/components/navbar.js
@@ -0,0 +1,23 @@
+
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        VERTO
+      </div>
+      <div className="login">
+      
+        <button>
+          Sell
+        </button>
+        <button>
+         log in
+        </button>
+      </div>
+    </nav>
+  )
+}
+
+export default navbar
